7a678489bc3b9ef1146056506dddc6c63c73d95b
AB95 <aboucher598@gmail.com>
2015-08-26 09:13:34 +0100

    Added folders and completed pi_to_nth_digit task

---
+++ Numbers/pi_to_nth_digit.py
@@ -0,0 +1,15 @@
+from __future__ import division
+from decimal import *
+
+
+# Currently accurate to 18 decimal places
+def pi_to_nth_digit(n):
+    getcontext().prec = 28
+    pi = Decimal(0)
+    for k in xrange(n):
+        pi += Decimal((16**-k)*((4/(8*k+1)) - (2/(8*k+4)) - (1/(8*k+5)) - (1/(8*k+6))))
+
+    if n == 1:
+        return str(pi)[0]
+    else:
+        return str(pi)[:n+1]
